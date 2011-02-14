4faeae39dc852ab389056ba63b2ffd85c180ab5f
XOMG OF MIGHTY GOO <xomg@wre.ath.cx>
2011-02-14 19:52:46 +0200

    on second thought, don't separate code
    you can refactor yourself to death later, with more use cases

--- src/main.c
+++ src/main.c
@@ -2,26 +2,12 @@
 // main.c: 
 
 #include "xomg_dmx_dimmer.h"  // board-specific
-#include "led.h"
-#include "thermo.h"
-#include "zc.h"
-#include "stp.h"
 #include "usart.h"            // dmx
 
-#define DMX_BAUD 250000                 // Hz
-#define USART_UBRR F_CPU/16/DMX_BAUD-1  // USART clock gen prescale
 
 void main (void)
 {
-    // ...
-
-    led_init();              // debugging
-    thermo_init();           // temperature control
-    zc_init();               // zero crossing
-    stp_init();              // serial to parallel
-    usart_init(USART_UBRR);  // dmx signals
-
-    // ...
+    board_init();  // internals and peripherals
 
     while (1) {
 	// ...
--- src/xomg_dmx_dimmer.h
+++ src/xomg_dmx_dimmer.h
@@ -3,10 +3,48 @@
 // settings specific to the board and used components
 
 #define F_CPU 16000000  // in case not set in Makefile
+#define DMX_BAUD 250000                 // Hz
+#define USART_UBRR F_CPU/16/DMX_BAUD-1  // USART clock gen prescale
 
-#define RED_LED_DDR    DDRD
-#define RED_LED_PORT   PORTD
-#define RED_LED        PD7
-#define GREEN_LED_DDR  DDRD
-#define GREEN_LED_PORT PORTD
-#define GREEN_LED      PD6
+// leds for debugging
+#define LED0_DDR  DDRD
+#define LED0_PORT PORTD
+#define LED0      PD7
+#define LED1_DDR  DDRD
+#define LED1_PORT PORTD
+#define LED1      PD6
+
+// comfortables
+#define output_low(port,pin) port &= ~(1<<pin)
+#define output_high(port,pin) port |= (1<<pin)
+#define set_input(portdir,pin) portdir &= ~(1<<pin)
+#define set_output(portdir,pin) portdir |= (1<<pin)
+
+
+#include <avr/io.h>
+#include "usart.h"
+
+
+void board_init (void) {
+    wdt_disable();
+
+    // debug leds
+    set_output(LED0_DDR, LED0);
+    set_output(LED1_DDR, LED1);
+
+    //thermo_init();           // temperature control
+    //zc_init();               // zero crossing
+    //stp_init();              // serial to parallel
+
+    // dmx signals
+    usart_init(USART_UBRR);
+
+    sei();
+}
+
+void blink (void) {
+    output_high(LED0_DDR, LED0);
+    delay_ms(50);
+    output_low(LED0_DDR, LED0);
+    delay_ms(50);
+}
