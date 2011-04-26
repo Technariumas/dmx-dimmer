a41191a60b8dddaabf0755023eaa7ff1dcc7c363
XOMG OF MIGHTY GOO <xomg@wre.ath.cx>
2011-04-26 15:35:54 +0300

    attiny2313 counts zero crossings
    code:
    
    zc_init() enables ZC interrupt and is essentially complete.
    
    ISR(INT0_vect) is a stub. Should determine how many cycles pass
    between two ZCs and divide this interval into [0 255] firing angles.
    
    timer1_init() enables cycle counting. This count will be used by
    ISR(INT0_vect).
    
    ISR (TIMER1_COMPA_vect) is just testing. Timer resetting should be performed by ISR(INT0_vect). Should be replaced by TIMER1_OVF1_vect, which happens on counter overflow, which means no zero crossing was detected in a long time and mains frequency is too low.
    
    Not Yet Implemented:
    
    Timer0, which gets a duration between two firing angles (from ISR(INT0_vect)) and fires dimmer channels.
    
    schem:
    
    Pull-up between op-amp and uC removed since uC's internal one is
    used.

--- src/dimmer/main.c
+++ src/dimmer/main.c
@@ -36,17 +36,74 @@ void delay_ns (uint16_t ns) {
     }
 }
 
+// zero crossing
+#include <avr/interrupt.h>
+
+#define ZC_DDR  DDRD
+#define ZC_PORT PORTD
+#define ZC      PD2
+
+inline void zc_init (void) {
+    set_input(ZC_DDR, ZC);
+    output_high(ZC_PORT, ZC);  // pullup
+
+    MCUCR |= _BV(ISC01);  // int on falling edge
+    GIMSK |= _BV(INT0);   // enable INT0 interrupt
+}
+
+uint8_t zc_count = 0;
+/* uint8_t outcount = 0; */
+
+// interrupt service routine: action to take on zero crossing
+ISR (INT0_vect, ISR_NOBLOCK) {
+    zc_count++;  // overflow no problem
+    /* if (zc_count >= 100) { */
+    /* 	//outcount++; */
+    /* 	zc_count = 0; */
+    /* } */
+}
+
+inline void timer1_init (void) {
+    OCR1AL = 100;
+    //OCR1AH = 0;
+
+    TCCR1B |= _BV(CS10);  // no prescaler
+}
+
+// interrupt: just for timer testing
+ISR (TIMER1_COMPA_vect, ISR_NOBLOCK) {
+    TCNT1L = 0;  // FIXME: can be done using TCCR1B instead
+}
+
 
 int main (void) {
+    wdt_disable();
+
+    // debug led
     set_output(DDRB, PB4);
 
-    while (1) {
-	wdt_reset();
+    output_low(PORTB, PB4);
+    delay_ms(200);
+    output_high(PORTB, PB4);
+    delay_ms(200);
+    output_low(PORTB, PB4);
+    delay_ms(200);
+    output_high(PORTB, PB4);
+    delay_ms(200);
 
-	output_low(PORTB, PB4);
-	delay_ms(500);
-	output_high(PORTB, PB4);
-	delay_ms(500);
+    zc_init();
+    timer1_init();
+    sei();
+
+    while (1) {
+	/* wdt_reset(); */
+
+	if (zc_count < 128) {
+	    output_low(PORTB, PB4);
+	}
+	else {
+	    output_high(PORTB, PB4);
+	}
     }
 
     return 1;
