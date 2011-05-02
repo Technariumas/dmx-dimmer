06d65ecfa21820b93757bae71bd83d5969ca5fa1
XOMG OF MIGHTY GOO <xomg@wre.ath.cx>
2011-05-02 19:16:31 +0300

    code: zc works!
    ...just a little regulation left - tested only on the highest trimpot
    setting.
    
    Timer/Counter0 does have output compare units. Even if it didn't,
    counting from 155 to 255 is no different than from 0 to 100.

--- src/dimmer/main.c
+++ src/dimmer/main.c
@@ -26,14 +26,13 @@ void delay_ms (uint16_t ms) {
     }
 }
 
-// FIXME: what if wdt is disabled?
 void delay_ns (uint16_t ns) {
     uint16_t delay_count = 1;  // cpu cycles in one ms?
     volatile uint16_t i;
 
     while (ns != 0) {
 	for (i = 0; i != delay_count; i++)
-	    wdt_reset();
+	    wdt_reset();  // FIXME: what if wdt is disabled?
 	ns--;
     }
 }
@@ -43,7 +42,6 @@ uint16_t zc_calibrate (uint16_t old, uint16_t new) {
     if (old == new) return old;
     if (old > new)  return (old - (old-new)/8);
     if (old < new) return (old + (new-old)/8);
-
     return new;  // should never happen
 }
 
@@ -54,6 +52,7 @@ uint16_t zc_calibrate (uint16_t old, uint16_t new) {
 
 // 
 inline void zc_init (void) {
+    // interrupt input pin
     set_input(ZC_DDR, ZC);     // redundant
     output_high(ZC_PORT, ZC);  // pullup
 
@@ -61,103 +60,99 @@ inline void zc_init (void) {
     GIMSK |= _BV(INT0);        // enable INT0 interrupt
 }
 
-uint8_t zc_count = 0;
-uint16_t zc_dur = 0;           // cycles between two ZCs
-uint8_t deg_dur = 0;           // cycles b/w two firing angles
-uint8_t angle = 255;           // dmx firing angle (counted backwards!)
+uint16_t zc_dur = 60000;  // cycles between two ZCs (ideal @ 50 Hz)
+uint8_t deg_dur = 234;    // cycles b/w two firing angles (ideal @ 50 Hz)
+uint8_t angle = 255;      // dmx firing angle (counted backwards!)
 
 // interrupt: action to take on zero crossing
 ISR (INT0_vect, ISR_NOBLOCK) {
-    uint16_t tmp_zc_dur = 0;   // degree duration (temporary)
     uint16_t old_zc_dur = zc_dur;
     uint8_t tcntl;
     uint8_t tcnth;
+
+    // disable counter0 ASAP
+    TCCR0B &= ~(_BV(CS00));
 
     // read counter (used later)
     // low byte must be read first
     tcntl = TCNT1L;
     tcnth = TCNT1H;
 
-    // reset counters (as soon as possible)
+    // reset counter1
     // high byte must be written first
     TCNT1H = 0;
     TCNT1L = 0;
-    angle = 255;
 
     // turn off outputs
     output_high(PORTB, PB4);  // TODO: don't use debug led for this
 
-    // read two ints into long int from input capture register
-    // low must be read first
-    tmp_zc_dur = tcntl;
+    // 
+    angle = 255;
+
+    // read two ints into long int
     zc_dur = tcnth;
     zc_dur = zc_dur << 8;
-    zc_dur += tmp_zc_dur;
+    zc_dur += tcntl;
 
-    // get average of previous and current
-    zc_dur = zc_calibrate(old_zc_dur, zc_dur);
+    // push old zc_dur towards new
+    //zc_dur = zc_calibrate(old_zc_dur, zc_dur);
 
     // determine degree duration (there are 256 degrees between 2 ZCs)
-    // high byte must be written first
     deg_dur = zc_dur/256;
-    OCR1AH = 0;
-    OCR1AL = deg_dur;
+    OCR0A = deg_dur;
 
-    zc_count++;  // overflow no problem (TODO: remove)
+    // re-enable counter0
+    TCCR0B |= _BV(CS00);
 }
 
-// 
-inline void counter1_init (void) {
-    // set longest possible degree duration (but only one byte is used)
-    // high byte must be written first
-    OCR1AH = 0;
-    OCR1AL = 0xff;
-
-    TIMSK |= _BV(TOIE1) | _BV(OCIE1A);  // interrupt on overflow
-    TCCR1B |= _BV(CS10);                // no prescaler
+// degree duration counter
+inline void counter0_init (void) {
+    TCNT0 = 0;            // reset counter
+    OCR0A = deg_dur;
+    TIMSK |= _BV(OCIE0A); // Output Compare A int
+    TCCR0B |= _BV(CS00);  // no prescaler
 }
 
 uint8_t channel[4];
 
-// interrupt: on every firing angle, fire matching channels and update
-// Output Compare A accordingly
-// NOTE: this must take less cycles than deg_dur
-ISR (TIMER1_COMPA_vect, ISR_NOBLOCK) {
-    uint8_t tmpl;
-    uint8_t tmph;
-    uint16_t cycles;  // cycles since zc
-    
+// interrupt: new firing angle reached
+// TODO: fire '255' from ZC int
+ISR (TIMER0_COMPA_vect, ISR_NOBLOCK) {
     // fire appropriate channels
     if (channel[0] >= angle) {
 	output_low(PORTB, PB4); // TODO: don't use debug led for this
     }
-    angle--;  // ASSERT angle != 0
 
-    // get current value of Output Compare
-    // low byte must be read first
-    tmpl = OCR1AL;
-    tmph = OCR1AH;
-    cycles = 256*tmph + tmpl;  // cycles since zc
+    if (angle != 0) angle--;
 
-    // calculate new value
-    cycles += deg_dur;
-    tmph = cycles/256;
-    tmpl = cycles & 0x00ff;
+    TCNT0 = 0;  // reset counter
+}
 
-    // set calculated value
+// zc duration counter
+inline void counter1_init (void) {
+    // reset counter
     // high byte must be written first
-    OCR1AH = tmph;
-    OCR1AL = tmpl;
+    TCNT1H = 0;
+    TCNT1L = 0;
+
+    TIMSK |= _BV(TOIE1);  // interrupt on overflow
+    TCCR1B |= _BV(CS10);  // no prescaler
 }
 
 // interrupt: maximum counter value reached, time interval between two
 // ZCs too long
-ISR (TIMER1_OVF_vect, ISR_BLOCK) {
-    while (1) {
-	output_low(PORTB, PB4);
-	delay_ms(500);
-	output_high(PORTB, PB4);
-	delay_ms(500);
+ISR (TIMER1_OVF_vect, ISR_NOBLOCK) {
+    /* while (1) { */
+    /* 	output_low(PORTB, PB4); */
+    /* 	delay_ms(500); */
+    /* 	output_high(PORTB, PB4); */
+    /* 	delay_ms(500); */
+    /* } */
+    if (PORTD & 0b00010000) {
+	output_low(PORTD, PD5);
+    }
+    else {
+	output_high(PORTD, PD5);
     }
 }
 
@@ -165,8 +160,15 @@ ISR (TIMER1_OVF_vect, ISR_BLOCK) {
 int main (void) {
     wdt_disable();
 
-    // debug led
-    set_output(DDRB, PB4);
+    // debug leds
+    set_output(DDRB, DDB4);
+    set_output(DDRD, DDD5);
+
+    // channel selector
+    set_input(DDRD, DDD0);
+    set_input(DDRD, DDD1);
+    output_high(PORTD, PD0);
+    output_high(PORTD, PD1);
 
     // blink: devboard ok
     /* output_low(PORTB, PB4); */
@@ -178,15 +180,15 @@ int main (void) {
     /* output_high(PORTB, PB4); */
     /* delay_ms(200); */
 
-    zc_init();
+
+    counter0_init();
     counter1_init();
+    zc_init();
     sei();
 
     while (1) {
-	// TODO: receive from Master
-	channel[0] = 255;
-
-	/* wdt_reset(); */
+	// TODO: receive channel values from Master
+	channel[0] = (PIND & 0b00000011) * 84 + 3;
 
 	/* if (zc_count < 128) { */
 	/*     output_low(PORTB, PB4); */
