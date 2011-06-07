de60dd609f6695dd0bf9942cb7b9db8954be37c8
XOMG OF MIGHTY GOO <xomg@wre.ath.cx>
2011-06-07 16:41:31 +0300

    code: transceiver: forgot to git add dmx.h

---
+++ src/transceiver/dmx.h
@@ -0,0 +1,25 @@
+// dmx.h
+// DMX-related, independent of USART or SPI
+
+#ifndef _DMX_H_
+#define _DMX_H_
+
+#include <inttypes.h>
+
+#define DMX_CHANNELS 12
+
+// DMX state machine possible states
+enum dmx_state_t {IDLE, BREAK, SKIP, DATA};
+
+// information about DMX data (but not the data itself)
+typedef struct {
+    enum dmx_state_t state;      // state machine
+    uint16_t         address;    // dmx address (of first channel)
+    uint16_t         slot;       // slot number counter
+    uint16_t         dataisnew;  // flag new data
+    uint8_t          status;     // usart status byte
+    uint8_t          data;       // usart data byte
+    uint8_t          chanval[DMX_CHANNELS];
+} dmx_t;
+
+#endif /* _DMX_H_ */
