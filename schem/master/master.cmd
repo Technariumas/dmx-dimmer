cfd15ad9f5829f1582b099fb2f36cf057c81e59f
shift <shift@wemakethings.net>
2012-01-13 23:27:36 +0200

    pcb: add Master PCB with placed components
    No routing done yet. Might need layer play.

---
+++ schem/master/master.cmd
@@ -0,0 +1,222 @@
+# Pin name action command file
+
+# Start of element PD5
+ChangePinName(PD5, 1, terminal)
+
+# Start of element PC5
+ChangePinName(PC5, 1, terminal)
+
+# Start of element PC4
+ChangePinName(PC4, 1, terminal)
+
+# Start of element R7
+ChangePinName(R7, 1, 1)
+ChangePinName(R7, 2, 2)
+
+# Start of element CONN_DMX_OUT
+ChangePinName(CONN_DMX_OUT, 5, 5)
+ChangePinName(CONN_DMX_OUT, 3, 3)
+ChangePinName(CONN_DMX_OUT, 1, 1)
+ChangePinName(CONN_DMX_OUT, 4, 4)
+ChangePinName(CONN_DMX_OUT, 2, 2)
+
+# Start of element R6
+ChangePinName(R6, 1, 1)
+ChangePinName(R6, 2, 2)
+
+# Start of element C3
+ChangePinName(C3, 2, 2)
+ChangePinName(C3, 1, 1)
+
+# Start of element CONN_DMX_IN
+ChangePinName(CONN_DMX_IN, 5, 5)
+ChangePinName(CONN_DMX_IN, 3, 3)
+ChangePinName(CONN_DMX_IN, 1, 1)
+ChangePinName(CONN_DMX_IN, 4, 4)
+ChangePinName(CONN_DMX_IN, 2, 2)
+
+# Start of element U2
+ChangePinName(U2, 5, GND)
+ChangePinName(U2, 8, VCC)
+ChangePinName(U2, 1, R)
+ChangePinName(U2, 7, B/DO*/RI*)
+ChangePinName(U2, 6, A/DO/RI)
+ChangePinName(U2, 4, D)
+ChangePinName(U2, 3, DE)
+ChangePinName(U2, 2, RE*)
+
+# Start of element C7
+ChangePinName(C7, 2, 2)
+ChangePinName(C7, 1, 1)
+
+# Start of element C6
+ChangePinName(C6, 2, 2)
+ChangePinName(C6, 1, 1)
+
+# Start of element Q1
+ChangePinName(Q1, 2, 2)
+ChangePinName(Q1, 1, 1)
+
+# Start of element U1
+ChangePinName(U1, 22, ADC7)
+ChangePinName(U1, 19, ADC6)
+ChangePinName(U1, 5, GND)
+ChangePinName(U1, 6, VCC)
+ChangePinName(U1, 13, "(OC1A/PCINT1) PB1")
+ChangePinName(U1, 14, "(SS/OC1B/PCINT2) PB2")
+ChangePinName(U1, 15, "(MOSI/OC2A/PCINT3) PB3")
+ChangePinName(U1, 16, "(MISO/PCINT4) PB4")
+ChangePinName(U1, 17, "(SCK/PCINT5) PB5")
+ChangePinName(U1, 18, AVCC)
+ChangePinName(U1, 20, AREF)
+ChangePinName(U1, 21, GND)
+ChangePinName(U1, 23, "(ADC0/PCINT8) PC0")
+ChangePinName(U1, 24, "(ADC1/PCINT9) PC1")
+ChangePinName(U1, 25, "(ADC2/PCINT10) PC2")
+ChangePinName(U1, 26, "(ADC3/PCINT11) PC3")
+ChangePinName(U1, 27, "(ADC4/SDA/PCINT12) PC4")
+ChangePinName(U1, 28, "(ADC5/SCL/PCINT13) PC5")
+ChangePinName(U1, 12, "PB0 (PCINT0/CLKO/ICP1)")
+ChangePinName(U1, 11, "PD7 (PCINT23/AIN1)")
+ChangePinName(U1, 10, "PD6 (PCINT22/OC0A/AIN0)")
+ChangePinName(U1, 9, "PD5 (PCINT21/OC0B/T1)")
+ChangePinName(U1, 8, "PB7 (PCINT7/XTAL2/TOSC2)")
+ChangePinName(U1, 7, "PB6 (PCINT6/XTAL1/TOSC1)")
+ChangePinName(U1, 3, GND)
+ChangePinName(U1, 4, VCC)
+ChangePinName(U1, 2, "PD4 (PCINT20/XCK/T0)")
+ChangePinName(U1, 1, "PD3 (PCINT19/OC2B/INT1)")
+ChangePinName(U1, 32, "PD2 (PCINT18/INT0)")
+ChangePinName(U1, 31, "PD1 (PCINT17/TXD)")
+ChangePinName(U1, 30, "PD0 (PCINT16/RXD)")
+ChangePinName(U1, 29, "PC6 (PCINT14/RESET)")
+
+# Start of element ISP_EN1
+ChangePinName(ISP_EN1, 1, 1)
+ChangePinName(ISP_EN1, 2, 2)
+
+# Start of element ISP1
+ChangePinName(ISP1, 6, 6)
+ChangePinName(ISP1, 7, 7)
+ChangePinName(ISP1, 8, 8)
+ChangePinName(ISP1, 9, 9)
+ChangePinName(ISP1, 10, 10)
+ChangePinName(ISP1, 5, 5)
+ChangePinName(ISP1, 1, 1)
+ChangePinName(ISP1, 4, 4)
+ChangePinName(ISP1, 3, 3)
+ChangePinName(ISP1, 2, 2)
+
+# Start of element C5
+ChangePinName(C5, 2, 2)
+ChangePinName(C5, 1, 1)
+
+# Start of element CONN_PANEL11
+ChangePinName(CONN_PANEL11, 15, 15)
+ChangePinName(CONN_PANEL11, 13, 13)
+ChangePinName(CONN_PANEL11, 11, 11)
+ChangePinName(CONN_PANEL11, 9, 9)
+ChangePinName(CONN_PANEL11, 16, 16)
+ChangePinName(CONN_PANEL11, 14, 14)
+ChangePinName(CONN_PANEL11, 12, 12)
+ChangePinName(CONN_PANEL11, 10, 10)
+ChangePinName(CONN_PANEL11, 7, 7)
+ChangePinName(CONN_PANEL11, 5, 5)
+ChangePinName(CONN_PANEL11, 3, 3)
+ChangePinName(CONN_PANEL11, 1, 1)
+ChangePinName(CONN_PANEL11, 8, 8)
+ChangePinName(CONN_PANEL11, 6, 6)
+ChangePinName(CONN_PANEL11, 4, 4)
+ChangePinName(CONN_PANEL11, 2, 2)
+
+# Start of element L1
+ChangePinName(L1, 2, 2)
+ChangePinName(L1, 1, 1)
+
+# Start of element C4
+ChangePinName(C4, 2, 2)
+ChangePinName(C4, 1, 1)
+
+# Start of element J3
+ChangePinName(J3, 1, 1)
+ChangePinName(J3, 2, 2)
+
+# Start of element CONN_OUT31
+ChangePinName(CONN_OUT31, 9, 9)
+ChangePinName(CONN_OUT31, 10, 10)
+ChangePinName(CONN_OUT31, 7, 7)
+ChangePinName(CONN_OUT31, 5, 5)
+ChangePinName(CONN_OUT31, 3, 3)
+ChangePinName(CONN_OUT31, 1, 1)
+ChangePinName(CONN_OUT31, 8, 8)
+ChangePinName(CONN_OUT31, 6, 6)
+ChangePinName(CONN_OUT31, 4, 4)
+ChangePinName(CONN_OUT31, 2, 2)
+
+# Start of element J2
+ChangePinName(J2, 1, 1)
+ChangePinName(J2, 2, 2)
+
+# Start of element CONN_OUT21
+ChangePinName(CONN_OUT21, 9, 9)
+ChangePinName(CONN_OUT21, 10, 10)
+ChangePinName(CONN_OUT21, 7, 7)
+ChangePinName(CONN_OUT21, 5, 5)
+ChangePinName(CONN_OUT21, 3, 3)
+ChangePinName(CONN_OUT21, 1, 1)
+ChangePinName(CONN_OUT21, 8, 8)
+ChangePinName(CONN_OUT21, 6, 6)
+ChangePinName(CONN_OUT21, 4, 4)
+ChangePinName(CONN_OUT21, 2, 2)
+
+# Start of element J1
+ChangePinName(J1, 1, 1)
+ChangePinName(J1, 2, 2)
+
+# Start of element CONN_OUT11
+ChangePinName(CONN_OUT11, 9, 9)
+ChangePinName(CONN_OUT11, 10, 10)
+ChangePinName(CONN_OUT11, 7, 7)
+ChangePinName(CONN_OUT11, 5, 5)
+ChangePinName(CONN_OUT11, 3, 3)
+ChangePinName(CONN_OUT11, 1, 1)
+ChangePinName(CONN_OUT11, 8, 8)
+ChangePinName(CONN_OUT11, 6, 6)
+ChangePinName(CONN_OUT11, 4, 4)
+ChangePinName(CONN_OUT11, 2, 2)
+
+# Start of element C2
+ChangePinName(C2, 2, 2)
+ChangePinName(C2, 1, 1)
+
+# Start of element C1
+ChangePinName(C1, 2, -)
+ChangePinName(C1, 1, +)
+
+# Start of element R5
+ChangePinName(R5, 1, 1)
+ChangePinName(R5, 2, 2)
+
+# Start of element R4
+ChangePinName(R4, 1, 1)
+ChangePinName(R4, 2, 2)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element Z2
+ChangePinName(Z2, 1, 1)
+ChangePinName(Z2, 2, 2)
+
+# Start of element Z1
+ChangePinName(Z1, 1, 1)
+ChangePinName(Z1, 2, 2)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
