42187b01b7f1b79ffb1628db33e3a720614da89e
rxdtxd <rxdtxd@wemakethings.net>
2012-01-28 13:05:27 +0200

    sch/slave: generated .pcb files

---
+++ sch/slave/slave.cmd
@@ -0,0 +1,171 @@
+# Pin name action command file
+
+# Start of element CONN_OUT12
+ChangePinName(CONN_OUT12, 9, 9)
+ChangePinName(CONN_OUT12, 10, 10)
+ChangePinName(CONN_OUT12, 7, 7)
+ChangePinName(CONN_OUT12, 5, 5)
+ChangePinName(CONN_OUT12, 3, 3)
+ChangePinName(CONN_OUT12, 1, 1)
+ChangePinName(CONN_OUT12, 8, 8)
+ChangePinName(CONN_OUT12, 6, 6)
+ChangePinName(CONN_OUT12, 4, 4)
+ChangePinName(CONN_OUT12, 2, 2)
+
+# Start of element J1
+ChangePinName(J1, 6, 6)
+ChangePinName(J1, 7, 7)
+ChangePinName(J1, 8, 8)
+ChangePinName(J1, 9, 9)
+ChangePinName(J1, 10, 10)
+ChangePinName(J1, 5, 5)
+ChangePinName(J1, 1, 1)
+ChangePinName(J1, 4, 4)
+ChangePinName(J1, 3, 3)
+ChangePinName(J1, 2, 2)
+
+# Start of element CONN_FAN21
+ChangePinName(CONN_FAN21, 1, 1)
+ChangePinName(CONN_FAN21, 2, 2)
+
+# Start of element CONN_FAN11
+ChangePinName(CONN_FAN11, 1, 1)
+ChangePinName(CONN_FAN11, 2, 2)
+
+# Start of element LED2
+ChangePinName(LED2, 2, K)
+ChangePinName(LED2, 1, A)
+
+# Start of element LED1
+ChangePinName(LED1, 2, K)
+ChangePinName(LED1, 1, A)
+
+# Start of element R7
+ChangePinName(R7, 1, 1)
+ChangePinName(R7, 2, 2)
+
+# Start of element R6
+ChangePinName(R6, 1, 1)
+ChangePinName(R6, 2, 2)
+
+# Start of element R5
+ChangePinName(R5, 1, 1)
+ChangePinName(R5, 2, 2)
+
+# Start of element C6
+ChangePinName(C6, 2, 2)
+ChangePinName(C6, 1, 1)
+
+# Start of element CONN_DIM11
+ChangePinName(CONN_DIM11, 1, 1)
+ChangePinName(CONN_DIM11, 2, 2)
+
+# Start of element CONN_DIM21
+ChangePinName(CONN_DIM21, 1, 1)
+ChangePinName(CONN_DIM21, 2, 2)
+
+# Start of element CONN_DIM31
+ChangePinName(CONN_DIM31, 1, 1)
+ChangePinName(CONN_DIM31, 2, 2)
+
+# Start of element CONN_DIM41
+ChangePinName(CONN_DIM41, 1, 1)
+ChangePinName(CONN_DIM41, 2, 2)
+
+# Start of element R4
+ChangePinName(R4, 1, 1)
+ChangePinName(R4, 2, 2)
+
+# Start of element C5
+ChangePinName(C5, 2, 2)
+ChangePinName(C5, 1, 1)
+
+# Start of element C4
+ChangePinName(C4, 2, 2)
+ChangePinName(C4, 1, 1)
+
+# Start of element Q1
+ChangePinName(Q1, 2, 2)
+ChangePinName(Q1, 1, 1)
+
+# Start of element U5
+ChangePinName(U5, 12, "(AIN0/PCINT0) PB0")
+ChangePinName(U5, 11, "PD6 (ICP)")
+ChangePinName(U5, 9, "PD5 (OC0B/T1)")
+ChangePinName(U5, 8, "PD4 (T0)")
+ChangePinName(U5, 7, "PD3 (INT1)")
+ChangePinName(U5, 6, "PD2 (CKOUT/XCK/INT0)")
+ChangePinName(U5, 3, "PD1 (TXD)")
+ChangePinName(U5, 2, "PD0 (RXD)")
+ChangePinName(U5, 5, "PA0 (XTAL1)")
+ChangePinName(U5, 4, "PA1 (XTAL2)")
+ChangePinName(U5, 1, "PA2 (\_RESET\_/dW)")
+ChangePinName(U5, 19, "(UCSK/SCL/PCINT7) PB7")
+ChangePinName(U5, 18, "(MISO/DO/PCINT6) PB6")
+ChangePinName(U5, 17, "(MOSI/DI/SDA/PCINT5) PB5")
+ChangePinName(U5, 16, "(OC1B/PCINT4) PB4")
+ChangePinName(U5, 15, "(OC1A/PCINT3) PB3")
+ChangePinName(U5, 14, "(OC0A/PCINT2) PB2")
+ChangePinName(U5, 13, "(AIN1/PCINT1) PB1")
+ChangePinName(U5, 10, GND)
+ChangePinName(U5, 20, VCC)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element C3
+ChangePinName(C3, 2, 2)
+ChangePinName(C3, 1, 1)
+
+# Start of element C2
+ChangePinName(C2, 2, -)
+ChangePinName(C2, 1, +)
+
+# Start of element C1
+ChangePinName(C1, 2, -)
+ChangePinName(C1, 1, +)
+
+# Start of element U4
+ChangePinName(U4, 3, 3)
+ChangePinName(U4, 2, 2)
+ChangePinName(U4, 1, 1)
+
+# Start of element R2
+ChangePinName(R2, 3, 3)
+ChangePinName(R2, 2, 2)
+ChangePinName(R2, 1, 1)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element U3
+ChangePinName(U3, 4, 4)
+ChangePinName(U3, 3, 3)
+ChangePinName(U3, 2, 2)
+ChangePinName(U3, 1, 1)
+
+# Start of element CONN_MAINS1
+ChangePinName(CONN_MAINS1, 3, E)
+ChangePinName(CONN_MAINS1, 2, N)
+ChangePinName(CONN_MAINS1, 1, P)
+
+# Start of element T1
+ChangePinName(T1, 3, 3)
+ChangePinName(T1, 4, 4)
+ChangePinName(T1, 1, 1)
+ChangePinName(T1, 2, 2)
+
+# Start of element U2
+ChangePinName(U2, 12, 12)
+ChangePinName(U2, 3, 3)
+ChangePinName(U2, 1, 1)
+ChangePinName(U2, 6, 6)
+ChangePinName(U2, 7, 7)
+
+# Start of element U1
+ChangePinName(U1, 4, 4)
+ChangePinName(U1, 3, 3)
+ChangePinName(U1, 2, 2)
+ChangePinName(U1, 1, 1)
