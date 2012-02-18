110d0bae0841c39f68e5e26ce6d31f93bed949c6
rxdtxd <rxdtxd@wemakethings.net>
2012-02-18 18:34:27 +0200

    src/slave: enable pull-up for interrupt pin to improve noise immunity
    PCINT3 is triggered on both edges, and Master simply toggles it when new data is available. The level on this pin stays in the previous state otherwise. When slave was unconnected to Master, it would get sporadic interrupts, probably due to noise. So enable a pull-up for this pin.

--- src/slave/spi.c
+++ src/slave/spi.c
@@ -17,6 +17,9 @@ inline void spi_slave_init (void) {
     output_high(SPI_OUT_PORT, SPI_OUT_CHAN0);
     output_high(SPI_OUT_PORT, SPI_OUT_CHAN1);
 
+    // debug: pull-up to remove noise on interrupt pin
+    output_high(SPI_OUT_PORT, SPI_OUT_SS);
+
     // enable external interrupt PCINT3 on PB3
     PCMSK |= _BV(PCINT3);
     GIMSK |= _BV(PCIE);
